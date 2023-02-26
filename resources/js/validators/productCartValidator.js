d6f1dfb513e347fc7d79867d1d57d7e640756424
Alvaro de la Rosa <alvarodelarosamora@gmail.com>
2023-02-26 21:55:43 +0100

    No permitir añadir mas de 100 unidades

--- resources/js/validators/productCartValidator.js
+++ resources/js/validators/productCartValidator.js
@@ -1,6 +1,6 @@
 import * as yup from 'yup'
 
 export const productCartValidator = yup.object().shape({
-    quantity: yup.number().required('Debe introducir una cantidad').integer('La cantidad debe ser un número entero').positive('La cantidad debe ser positiva'),
+    quantity: yup.number().required('Debe introducir una cantidad').integer('La cantidad debe ser un número entero').positive('La cantidad debe ser positiva').max(100, 'Máximo 100 unidades'),
     productId: yup.number().required('El producto es obligatorio').integer('El ID del producto debe ser un número entero').positive('El ID del producto debe ser positivo')
 })
